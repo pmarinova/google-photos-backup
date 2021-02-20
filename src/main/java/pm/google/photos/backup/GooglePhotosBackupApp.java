a2871b8e7f48739177447c1f0c7265921c895f1c
Plamena Marinova <plamena.marinova@gmail.com>
2021-02-20 15:33:30 +0200

    accept command line arguments

--- src/main/java/pm/google/photos/backup/GooglePhotosBackupApp.java
+++ src/main/java/pm/google/photos/backup/GooglePhotosBackupApp.java
@@ -1,10 +1,18 @@
 package pm.google.photos.backup;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
-import java.time.Month;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import com.google.auth.Credentials;
 
@@ -13,16 +21,66 @@ import pm.google.photos.backup.auth.GoogleAuthFlow;
 
 public class GooglePhotosBackupApp {
 
+	private static final String CMD_OPTION_BACKUP_DIR = "backup_dir";
+	private static final String CMD_OPTION_BACKUP_DIR_DEFAULT = "backup";
+
+	private static final String CMD_OPTION_CLIENT_SECRET = "client_secret";
+	private static final String CMD_OPTION_CLIENT_SECRET_DEFAULT = "client_secret.json";
+
+	private static final String CMD_OPTION_START_DATE = "start_date";
+	private static final String CMD_OPTION_END_DATE = "end_date";
+
+
 	public static void main(String[] args) {
 
-		File backupDir = new File("backup");
+		Options options = new Options();
+		options.addOption(CMD_OPTION_BACKUP_DIR, 	true, "the backup directory where photos will be downloaded");
+		options.addOption(CMD_OPTION_CLIENT_SECRET, true, "the path to the client_secret.json file");
+		options.addOption(CMD_OPTION_START_DATE, 	true, "optional start date (YYYY-MM-DD), if specified only photos created after this date will be backed up");
+		options.addOption(CMD_OPTION_END_DATE, 		true, "optional end date (YYYY-MM-DD), if specified only photos created before this date will be backed up");
+
+		try {
+			CommandLine cmdLine = new DefaultParser().parse(options, args);
+
+			File backupDir = getBackupDir(cmdLine);
+			File clientSecretFile = getClientSecretFile(cmdLine);
+			LocalDate startDate = getStartDate(cmdLine);
+			LocalDate endDate = getEndDate(cmdLine);
+
+			runGooglePhotosBackup(backupDir, clientSecretFile, startDate, endDate);
+			System.exit(0);
+		}
+		catch (ParseException ex) {
+			ex.printStackTrace();
+			printHelp(options);
+			System.exit(1);
+		}
+		catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			System.exit(2);
+		}
+	}
+
+
+	public static void runGooglePhotosBackup(
+			File backupDir,
+			File clientSecretFile,
+			LocalDate startDate,
+			LocalDate endDate) {
+
+		System.out.println("Backup directory is " + backupDir.getAbsolutePath());
+		System.out.println("Client secret file is " + clientSecretFile.getAbsolutePath());
+
+		System.out.println(String.format("Backing up photos from %s till %s",
+				(startDate != null ? startDate : "the beginning"),
+				(endDate != null ? endDate : "now")));
+
 
 		File dataStoreDir = new File(backupDir, ".data_store");
 		File credentialsDataStore = new File(dataStoreDir, "credentials");
 		File photosIndexDataStore = new File(dataStoreDir, "index");
 
-		File clientSecretJsonFile = new File("client_secret.json");
-		GoogleAuthFlow authFlow = new GoogleAuthFlow(clientSecretJsonFile, credentialsDataStore);
+		GoogleAuthFlow authFlow = new GoogleAuthFlow(clientSecretFile, credentialsDataStore);
 
 		String userId = System.getProperty("user.name");
 		List<String> scopes = Arrays.asList("https://www.googleapis.com/auth/photoslibrary.readonly");
@@ -40,7 +98,8 @@ public class GooglePhotosBackupApp {
 			photosIndex.initialize();
 
 			PhotosBackupRunner backup = new PhotosBackupRunner(photosLibrary, photosIndex, backupDir);
-			backup.setStartDate(LocalDate.of(2020, Month.OCTOBER, 1));
+			backup.setStartDate(startDate);
+			backup.setEndDate(endDate);
 			backup.run();
 
 		} finally {
@@ -49,5 +108,57 @@ public class GooglePhotosBackupApp {
 			if (photosIndex != null)
 				photosIndex.close();
 		}
+	}
+
+
+	private static File getBackupDir(CommandLine cmdLine) {
+
+		File backupDir = new File(cmdLine.getOptionValue(
+				CMD_OPTION_BACKUP_DIR, CMD_OPTION_BACKUP_DIR_DEFAULT));
+		try {
+			Files.createDirectories(backupDir.toPath());
+			return backupDir;
+		} catch (IOException ex) {
+			throw new RuntimeException("Invalid backup directory: " + backupDir.getAbsolutePath(), ex);
+		}
+	}
+
+
+	private static File getClientSecretFile(CommandLine cmdLine) {
+
+		File clientSecretFile = new File(cmdLine.getOptionValue(
+				CMD_OPTION_CLIENT_SECRET, CMD_OPTION_CLIENT_SECRET_DEFAULT));
+
+		if (!clientSecretFile.exists())
+			throw new RuntimeException("Invalid client secret file: " + clientSecretFile.getAbsolutePath());
+
+		return clientSecretFile;
+	}
+
+
+	private static LocalDate getStartDate(CommandLine cmdLine) {
+		return cmdLine.hasOption(CMD_OPTION_START_DATE) ?
+				parseLocalDate(cmdLine.getOptionValue(CMD_OPTION_START_DATE)) : null;
+	}
+
+
+	private static LocalDate getEndDate(CommandLine cmdLine) {
+		return cmdLine.hasOption(CMD_OPTION_END_DATE) ?
+				parseLocalDate(cmdLine.getOptionValue(CMD_OPTION_END_DATE)) : null;
+	}
+
+
+	private static LocalDate parseLocalDate(String date) {
+		try {
+			return LocalDate.parse(date);
+		} catch (DateTimeParseException ex) {
+			throw new RuntimeException("Invalid date: " + date);
+		}
+	}
+
+
+	private static void printHelp(Options options) {
+		HelpFormatter help = new HelpFormatter();
+		help.printHelp("google-photos-backup", options);
 	}
 }
